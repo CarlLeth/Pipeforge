cdf8152d79aeb3335041290b1373c5589d416feb
Carl Leth <CarlLeth@users.noreply.github.com>
2026-06-21 10:40:34 -0600

    Added tests for AccumulatingPipe, including demonstrating use of WeakRefs

--- test/pipes.spec.ts
+++ test/pipes.spec.ts
@@ -58,3 +58,78 @@ describe('DelayingPipe', () => {
         }
     });
 });
+
+describe('AccumulatingPipe', () => {
+    it('accumulates values using a state-updating function', async () => {
+        vi.useFakeTimers();
+
+        let result: number | undefined = undefined;
+
+        const state = Pipe.state<number>();
+        const sum = state.fold((state, next) => state + next, 0);
+
+        sum.subscribe(val => result = val);
+        
+        expect(sum.get()).toBe(0);
+
+        state.set(3);
+        await vi.advanceTimersByTimeAsync(10);
+        expect(result).toBe(3);
+
+        state.set(5);
+        await vi.advanceTimersByTimeAsync(10);
+        expect(result).toBe(8);
+
+        // An interesting case happens here.
+        // The AccumulatingPipe will accept simultaneous values if they are present.
+        // However, other pipes may handle simultaneous values differently.
+        // Simultaneous values are a hard problem in reactive programming, and care should be taken
+        // to understand behavior (and expectations) when they occur. In this exact case, the user
+        // probably expects that the 11 value will be included in the accumulation, so we will
+        // leave that as an expectation in this test.
+        state.set(11);
+        state.set(2);
+
+        await vi.advanceTimersByTimeAsync(10);
+        expect(result).toBe(21);
+        expect(sum.get()).toBe(21);
+    });
+
+    it('can accumulate values before any listeners are subscribed', async () => {
+        // We are trying to simplify reasoning about reactive programming by making their behavior
+        // independent of whether they are subscribed to or not. Accumulating values comes with
+        // common pitfalls in other reactive libraries, including different subscribers receiving different values
+        // and values being dropped if nobody is subscribed. Pipeforge uses WeakRefs to maintain functionality
+        // when no subscribers are active. This causes extra calls until obsolete pipes are garbage collected, but
+        // significantly simplifies reasoning about the state of each pipe.
+
+        vi.useFakeTimers();
+
+        const state = Pipe.state<number>();
+        const sum = state.fold((state, next) => state + next, 0);
+
+        expect(sum.get()).toBe(0);
+
+        state.set(3);
+        await vi.advanceTimersByTimeAsync(10);
+        expect(sum.get()).toBe(3);
+
+        state.set(5);
+        await vi.advanceTimersByTimeAsync(10);
+        expect(sum.get()).toBe(8);
+
+        state.set(11);
+        state.set(2);
+
+        await vi.advanceTimersByTimeAsync(10);
+        expect(sum.get()).toBe(21);
+
+        let result: number | undefined = undefined;
+        sum.doOnce(val => result = val);
+
+        // While it might work immediately, there is no requirement that subscriptions fire immediately.
+        // Pipes are allowed a small, fixed number of cycles to propegate updates.
+        await vi.advanceTimersByTimeAsync(1);
+        expect(result).toBe(21);
+    });
+});
